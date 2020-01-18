21ad3de6b944756cff7e97936fa414dd783c6730
bill guedel <wsguede@gmail.com>
2020-01-18 13:50:48 -0600

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# System wide zsh setup
+
+This is a quick script to install oh-my-zsh globally.
+This will set up new users, and fix root
+
+
+
+### One line runner!
+```sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/wsguede/ohmyzsh/master/tools/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# @author Bill Guedel <wsguede@gmail.com>
+#
+# This is what should be run on a fresh install. This should be run as sudo
+
+
+# Install zsh first
+apt update
+apt install -y zsh git curl
+apt autoremove -y
+apt clean
+
+
+# Change the skel for a new user
+rm -f /etc/skel/.profile /etc/skel/.bash*
+curl -fsSLo /etc/skel/.zshrc "https://raw.githubusercontent.com/wsguede/ohmyzsh/master/.zshrc"
+mkdir -p /etc/skel/.oh-my-zsh/plugins /etc/skel/.oh-my-zsh/themes
+chmod -R 644 /etc/skel
+chown root:root /etc/skel
+# change root to have zsh file
+rm -f /root/.profile /root/.bash*
+yes | cp -R /etc/skel/ /root/
+
+
+# Change adduser to use zsh by default. Also change root user to use zsh
+sed -i 's#^\(DSHELL\s*=\s*\).*$#\1/bin/zsh#' /etc/adduser.conf
+useradd -s /bin/zsh root
+
+
+# install oh-my-zsh globally
+ZSH="/etc/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# add syntax highlighting to the global config. This might be a bad idea. Will probably never get updated, might get overwritten on upgrade
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH}/plugins/zsh-syntax-highlighting
